a17921dd164c39e0002442712420823848c69cd6
shogo koto <s-kotoh@cec-ltd.co.jp>
2017-02-28 10:02:36 +0900

    initial commit

---
+++ XamarinTest.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinTest.Android
+{
+    [Activity(Label = "XamarinTest.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
